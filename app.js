aece30d8e90969d42040f9d9560b6ba125545fe4
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2018-08-08 16:19:16 -0500

    Uso de parámetros por rutas
    En esta sección se tocó el tema de parámetros por medio de rutas,
    es decir que de acuerdo a una ruta dada el servidor puede responder
    y manipular esa información envíada.
    
    Únicamente se agregó una nueva ruta para obtener los datos de un
    estudiante en específico, que lo busca por su id, y si no lo encuentra
    entonces manda un mensaje al navegador.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import students from './data/students.json';
 import careers from './data/careers.json';
 import express from 'express';
+import _ from 'lodash';
 
 const server = express();
 const PORT = 3000;
@@ -17,6 +18,18 @@ server.get('/', (req, res) => res.send('Hola mundo desde Express'));
 
 // api/v1/students.json
 server.get(buildUrl('v1', 'students'), (req, res) => res.json(students));
+
+// api/v1/student/:id
+server.get(buildUrl('v1', 'student/:id'), (req, res) => {
+    const student = _.find(students, student => student.id === req.params.id);
+
+    if (student) {
+        res.json(student);
+    } else {
+        res.send(`No existe el estudiante con el id: ${req.params.id}`);
+    }
+    res.end();
+});
 
 // api/v1/careers.json
 server.get(buildUrl('v1', 'careers'), (req, res) => res.json(careers));

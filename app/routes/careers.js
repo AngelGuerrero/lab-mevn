29e3ba482b91058342ca72b9f87b9be3c1c00dfe
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2023-10-05 00:00:00 -0600

    feat: updating files, formatting and deleting node-sass

--- app/routes/careers.js
+++ app/routes/careers.js
@@ -1,9 +1,9 @@
-import careers from '../data/careers.json';
-import express from 'express';
+import careers from "../data/careers.json";
+import express from "express";
 
 var router = express.Router();
 
 // api/v1/careers.json
-router.get('/', (req, res) => res.json(careers));
+router.get("/", (req, res) => res.json(careers));
 
 export default router;
--- system/database.js
+++ system/database.js
@@ -1,41 +1,41 @@
-import readYaml from 'read-yaml';
-import mongoose from 'mongoose';
+import readYaml from "read-yaml";
+import mongoose from "mongoose";
 
 module.exports = class Connection {
+  constructor() {
+    this.config_path = "./app/config/database.yaml";
+    this.prefix_url = "mongodb://";
+    this.db = null;
 
-    constructor() {
-        this.config_path = './app/config/database.yaml';
-        this.prefix_url = 'mongodb://';
-        this.db = null;
+    this.connect();
+  }
 
-        this.connect(); 
+  readData() {
+    var config = readYaml.sync(this.config_path);
+
+    if (config) {
+      this.user = config.database.user;
+      this.password = config.database.password;
+
+      this.db_url = `${this.prefix_url}${this.user}:${this.password}${config.database.url}`;
+    } else {
+      console.log("Error trying read the database.yaml");
     }
+  }
 
-    readData() {
-        var config = readYaml.sync(this.config_path);
+  connect() {
+    this.readData();
 
-        if (config) {
-            this.user = config.database.user;
-            this.password = config.database.password;
-
-            this.db_url = `${this.prefix_url}${this.user}:${this.password}${config.database.url}`;
+    mongoose.connect(this.db_url, { useNewUrlParser: true }).then(
+      () => console.log(`Successfully database connection.`),
+      (err) => {
+        console.log("Something went wrong trying to connect to the database.");
+        if (!this.user || !this.password) {
+          console.log("There was not provided a username or password");
         } else {
-            console.log('Error trying read the database.yaml');
+          console.log(`Error: ${err.code}. Message: ${err.errmsg}`);
         }
-    }
-
-    connect() {
-        this.readData();
-        
-        mongoose.connect(this.db_url, { useNewUrlParser: true })
-                .then(() => console.log(`Successfully database connection.`),
-                      (err) => {
-                          console.log("Something went wront trying to connect to the database.");
-                          if (!this.user || !this.password) {
-                              console.log('There was not provided a username or password');
-                          } else {
-                              console.log(`Error: ${err.code}. Message: ${err.errmsg}`);
-                          }
-                    });
-    }
-}
+      }
+    );
+  }
+};
